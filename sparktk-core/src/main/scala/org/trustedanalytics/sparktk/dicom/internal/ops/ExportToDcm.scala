9e391f3078cfd07711cbbfffc5253dfdc536dc54
Joyesh Mishra <joyesh.mishra@intel.com>
2016-10-06 17:46:09 -0700

    Fix

--- sparktk-core/src/main/scala/org/trustedanalytics/sparktk/dicom/internal/ops/ExportToDcm.scala
+++ sparktk-core/src/main/scala/org/trustedanalytics/sparktk/dicom/internal/ops/ExportToDcm.scala
@@ -1,18 +1,19 @@
 package org.trustedanalytics.sparktk.dicom.internal.ops
 
-import java.io.File
+import java.io.{ DataOutputStream, ByteArrayOutputStream, ByteArrayInputStream, File }
 import java.net.URI
+import javax.xml.parsers.{ SAXParser, SAXParserFactory }
 
-import org.apache.commons.io.FileUtils
+import org.apache.commons.io.{ IOUtils, FileUtils }
 import org.apache.commons.lang3.StringUtils
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.permission.{ FsAction, FsPermission }
-import org.apache.hadoop.fs.{ Path, FileSystem }
+import org.apache.hadoop.fs.{ FSDataOutputStream, Path, FileSystem }
 import org.apache.spark.mllib.linalg.DenseMatrix
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
-import org.dcm4che3.data.{ VR, Tag }
-import org.dcm4che3.io.DicomOutputStream
+import org.dcm4che3.data.{ UID, Attributes, VR, Tag }
+import org.dcm4che3.io.{ ContentHandlerAdapter, DicomOutputStream }
 import org.dcm4che3.tool.xml2dcm.Xml2Dcm
 import org.trustedanalytics.sparktk.dicom.internal.{ DicomState, DicomSummarization, BaseDicom }
 
@@ -36,7 +37,44 @@ case class ExportToDcm(path: String) extends DicomSummarization[Unit] {
   }
 }
 
-object ExportToDcm {
+object ExportToDcm extends Serializable {
+
+  def getAttrFromMetadata(metadata: String): org.dcm4che3.data.Attributes = {
+    val attrs: Attributes = new Attributes
+    val ch: ContentHandlerAdapter = new ContentHandlerAdapter(attrs)
+    val f: SAXParserFactory = SAXParserFactory.newInstance
+    val p: SAXParser = f.newSAXParser
+
+    val metadataByteStream = new ByteArrayInputStream(metadata.getBytes)
+    p.parse(metadataByteStream, ch)
+    return attrs
+  }
+
+  def writeToHdfs(dcmAttributes: Attributes, path: String): Unit = {
+    val b = new ByteArrayOutputStream()
+    val d = new DicomOutputStream(b, UID.ExplicitVRLittleEndian)
+    dcmAttributes.writeTo(d)
+
+    val bytes = b.toByteArray
+
+    val exportFile = s"${java.lang.System.currentTimeMillis()}.dcm"
+
+    //        //write to /tmp directory and from there copy to hdfs
+    //        val exportFile: File = File.createTempFile(s"export", ".dcm")
+    //        exportFile.deleteOnExit()
+    //        val dos: DicomOutputStream = new DicomOutputStream(exportFile)
+    //        dcmAttributes.writeTo(dos)
+    //
+    //        //copy to given path from /tmp
+    val dcmTargetFileName = StringUtils.join(path, "/", exportFile)
+    val hdfsPath = new Path(dcmTargetFileName)
+    val hdfsFileSystem = FileSystem.get(new URI(dcmTargetFileName), new Configuration())
+    val hdfsOutputStream: FSDataOutputStream = hdfsFileSystem.create(hdfsPath)
+    hdfsOutputStream.write(bytes)
+    hdfsOutputStream.hflush()
+    hdfsOutputStream.close()
+
+  }
 
   def exportToDcmFile(metadataRdd: RDD[Row], pixeldataRdd: RDD[Row], path: String) = {
 
@@ -44,37 +82,26 @@ object ExportToDcm {
 
     zipMetadataPixelData.cache()
 
-    zipMetadataPixelData.foreach {
+    val attrs = zipMetadataPixelData.foreach {
       case (metadata, pixeldata) => {
 
         val oneMetadata = metadata(1).toString
-        var onePixeldata: DenseMatrix = pixeldata(1).asInstanceOf[DenseMatrix]
+        val onePixeldata: DenseMatrix = pixeldata(1).asInstanceOf[DenseMatrix]
 
-        val tmpFile: File = File.createTempFile(s"dicom-xml-temp", ".xml")
-        FileUtils.writeByteArrayToFile(tmpFile, oneMetadata.getBytes)
-        tmpFile.deleteOnExit()
+        val dcmAttributes = getAttrFromMetadata(oneMetadata)
 
-        val dcmAttributes = Xml2Dcm.parseXML(tmpFile.getAbsolutePath)
-
-        onePixeldata = onePixeldata.transpose
+        //        onePixeldata = onePixeldata.transpose
         val pixel = onePixeldata.toArray.map(x => x.toInt)
+
         dcmAttributes.setInt(Tag.PixelData, VR.OW, pixel: _*) //inserting modified pixeldata
         dcmAttributes.setInt(Tag.Rows, VR.US, onePixeldata.numRows)
         dcmAttributes.setInt(Tag.Columns, VR.US, onePixeldata.numCols)
 
-        //write to /tmp directory and from there copy to hdfs
-        val exportFile: File = File.createTempFile(s"export", ".dcm")
-        exportFile.deleteOnExit()
-        val dos: DicomOutputStream = new DicomOutputStream(exportFile)
-        dcmAttributes.writeTo(dos)
+        writeToHdfs(dcmAttributes, path)
 
-        //copy to given path from /tmp
-        val dcmTargetFileName = StringUtils.join(path,  "/", exportFile.getName)
-        val hdfsPath = new Path(dcmTargetFileName)
-        val hdfsFileSystem = FileSystem.get(new URI(dcmTargetFileName), new Configuration())
-        val localTmpPath = new Path(exportFile.getAbsolutePath)
-        hdfsFileSystem.copyFromLocalFile(false, true, localTmpPath, hdfsPath)
-        hdfsFileSystem.setPermission(hdfsPath, new FsPermission(FsAction.ALL, FsAction.ALL, FsAction.NONE))
+        //        val localTmpPath = new Path(exportFile.getAbsolutePath)
+        //        hdfsFileSystem.copyFromLocalFile(false, true, localTmpPath, hdfsPath)
+        //        hdfsFileSystem.setPermission(hdfsPath, new FsPermission(FsAction.ALL, FsAction.ALL, FsAction.NONE))
       }
     }
 
--- sparktk-core/src/test/scala/org/trustedanalytics/sparktk/dicom/internal/ops/ExportToDcmTest.scala
+++ sparktk-core/src/test/scala/org/trustedanalytics/sparktk/dicom/internal/ops/ExportToDcmTest.scala
@@ -10,9 +10,9 @@ class ExportToDcmTest extends TestingSparkContextWordSpec with Matchers {
 
   "ExportToDcm" should {
     "create new .dcm from metadata and pixeldata and export to specified hdfs/local path" in {
-      val dicom = importDcm(sparkContext, "../integration-tests/datasets/dicom_uncompressed")
+      val dicom = importDcm(sparkContext, "../spark-tk/integration-tests/datasets/dicom_uncompressed")
 
-      val destPath = "../integration-tests/tests/sandbox/dicom_export"
+      val destPath = "../spark-tk/integration-tests/tests/sandbox/dicom_export"
       dicom.exportToDcm(destPath)
 
       val export_status = new File(destPath).exists()
